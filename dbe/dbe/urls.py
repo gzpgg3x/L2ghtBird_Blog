4f9945a365b8cf76e227bd9f8206eb35fb8f6413
Forrest Pan <gzpgg3x@yahoo.com>
2013-04-23 11:45:18 -0400

    AdminAddPost_Worked

---
+++ dbe/dbe/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls import patterns, include, url
+# from blog.views import main
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+# urlpatterns = patterns('',
+#     # Examples:
+#     # url(r'^$', 'dbe.views.home', name='home'),
+#     # url(r'^dbe/', include('dbe.foo.urls')),
+
+#     # Uncomment the admin/doc line below to enable admin documentation:
+#     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+#     # Uncomment the next line to enable the admin:
+#     # url(r'^admin/', include(admin.site.urls)),
+
+# )
+
+urlpatterns = patterns('blog.views',
+    url(r'^admin/', include(admin.site.urls)),
+    (r'^main/$', "main"),
+)
